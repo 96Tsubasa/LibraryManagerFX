4619e8805a3b9d6e5ddf38d428435bc7a6cdd047
LeDinhNhatLinh <skytrieuquoclong3639@gmail.com>
2024-11-25 08:04:27 +0700

    fix:

--- src/application/User.java
+++ src/application/User.java
@@ -85,7 +85,7 @@ public class User {
 
     /** Setter for role attribute. */
     public void setRole(String role) {
-        if (role == null || role != ADMIN && role != NORMAL_USER) {
+        if (role == null || !role.equals(ADMIN) && !role.equals(NORMAL_USER)) {
             throw new IllegalArgumentException("Role cannot be null.");
         }
         this.role = role;
