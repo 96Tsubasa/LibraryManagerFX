389fba01f055d3da884d75cd04d6f809b486933f
Vũ Văn Mạnh <manhvu20012005@gmail.com>
2024-12-02 11:09:09 +0700

    Add "profile" to show user information

--- src/application/controllers/MemberDashboardController.java
+++ src/application/controllers/MemberDashboardController.java
@@ -1,5 +1,6 @@
 package application.controllers;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
@@ -7,6 +8,7 @@ import java.util.ResourceBundle;
 
 import application.logic.Book;
 import application.logic.LibrarySystem;
+import application.logic.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +21,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -27,6 +33,8 @@ public class MemberDashboardController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    String currentPane = "browseBook";
     
     LibrarySystem librarySystem;
 
@@ -34,10 +42,28 @@ public class MemberDashboardController implements Initializable {
     private GridPane bookContainer;
 
     @FXML
-    private Button browseBook;
+    private AnchorPane browseBook;
 
     @FXML
     private Button logout;
+
+    @FXML
+    private AnchorPane profile;
+
+    @FXML
+    private Label profileEmail;
+
+    @FXML
+    private Label profileID;
+
+    @FXML
+    private Label profileRole;
+
+    @FXML
+    private Label profileUsername;
+
+    @FXML
+    private ImageView profileImage;
 
     @FXML
     private void logout(ActionEvent event) throws IOException {
@@ -53,6 +79,29 @@ public class MemberDashboardController implements Initializable {
             stage.setScene(scene);
             stage.show();
         }
+    }
+
+    public void disablePane() {
+        switch(currentPane) {
+            case "browseBook":
+                browseBook.setVisible(false);
+                break;
+            case "profile":
+                profile.setVisible(false);
+                break;
+        }
+    }
+
+    public void switchToBrowseBook() {
+        disablePane();
+        currentPane = "browseBook";
+        browseBook.setVisible(true);
+    }
+
+    public void switchToProfile() {
+        disablePane();
+        currentPane = "profile";
+        profile.setVisible(true);
     }
 
     private void loadBookContainer() {
@@ -79,9 +128,28 @@ public class MemberDashboardController implements Initializable {
         }
     }
 
+    private void loadProfileData() {
+        User currentUser = LoginController.currentUser;
+        profileID.setText("User ID: " + String.valueOf(currentUser.getUserId()));
+        profileUsername.setText("Username: " + currentUser.getUsername());
+        profileEmail.setText("Email: " + currentUser.getEmail());
+        profileRole.setText("Role: " + currentUser.getRole());
+        if (currentUser.getImageUser() != null) {
+            profileImage.setImage(convertBytesToImage(currentUser.getImageUser()));
+        } else {
+            profileImage.setImage(new Image(getClass().getResource("/resources/image/avatar.png").toExternalForm()));
+        }
+    }
+
+    private Image convertBytesToImage(byte[] bytes) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        return new Image(byteArrayInputStream);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         librarySystem = LibrarySystem.getInstance();
         loadBookContainer();
+        loadProfileData();
     }
 }
