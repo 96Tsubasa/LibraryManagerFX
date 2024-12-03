86cf329542d436ef1f166d07602132426c9018a8
Vũ Văn Mạnh <manhvu20012005@gmail.com>
2024-12-03 10:57:41 +0700

    show book rating in "browse book"

--- src/application/controllers/BookController.java
+++ src/application/controllers/BookController.java
@@ -2,6 +2,7 @@ package application.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -13,9 +14,12 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import application.logic.Book;
+import application.logic.LibrarySystem;
 
 public class BookController {
-    Book book;
+    private Book book;
+    private LibrarySystem librarySystem;
+    private final int maxRating = 5;
 
     @FXML
     private Label bookAuthor;
@@ -26,12 +30,32 @@ public class BookController {
     @FXML
     private Label bookTitle;
 
+    @FXML
+    private ImageView rating;
+
     public void setData(Book book) {
         this.book = book;
+        librarySystem = LibrarySystem.getInstance();
         Image image = convertBytesToImage(book.getCoverImage());
         bookCover.setImage(image);
         bookAuthor.setText(book.getAuthorsAsString());
         bookTitle.setText(book.getTitle());
+        showRating();
+    }
+
+    private void showRating() {
+        if (librarySystem.getRatings() == null) {
+            rating.setImage(new Image("/resources/image/norating.png"));
+            return;
+        }
+
+        Image image = new Image(getClass().getResource("/resources/image/rating.png").toExternalForm());
+        double percentage = librarySystem.getAvgBookRating(book.getBookId()) / maxRating;
+        double width = image.getWidth() * percentage;
+        double height = image.getHeight();
+        Rectangle2D viewport = new Rectangle2D(0, 0, width, height);
+        rating.setImage(image);
+        rating.setViewport(viewport);
     }
 
     private Image convertBytesToImage(byte[] bytes) {
