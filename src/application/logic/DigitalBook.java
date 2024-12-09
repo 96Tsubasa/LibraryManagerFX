41d9f2ecdfe36cf1298e363f05b54b279b3b497c
Hifumiii <hoangleminhminecraft@gmail.com>
2024-12-09 20:13:15 +0700

    add subclass for Book

---
+++ src/application/logic/DigitalBook.java
@@ -0,0 +1,20 @@
+package application.logic;
+
+public class DigitalBook extends Book {
+    private String bookUrl;
+
+    public DigitalBook(long bookId, String title, String[] authors, String publisher,
+                       int publicationYear, String[] genres, String description,
+                       byte[] coverImage, String isbn, String bookUrl) {
+        super(bookId, title, authors, publisher, publicationYear, genres, description, coverImage, isbn);
+        this.bookUrl = bookUrl;
+    }
+
+    public String getBookUrl() {
+        return bookUrl;
+    }
+
+    public void setBookUrl(String bookUrl) {
+        this.bookUrl = bookUrl;
+    }
+}
